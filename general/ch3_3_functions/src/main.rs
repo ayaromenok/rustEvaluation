36b72d69724231300abe0ff991b823c5c2b49fea
Andrey Yaromenok/PC <ayaromenok@gmail.com>
2019-09-03 13:30:53 +0300

    #4 functions

---
+++ general/ch3_3_functions/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    println!("ch3_3_functions");
+    another_function(5, -5);
+}
+
+fn another_function( x:i32, y:i32) {
+    println!("values from another_function: x: {}, y: {}", x, y);
+}
