9373edd61812dc5b839246397b6e158c302fc16f
Andrey Yaromenok/PC <ayaromenok@gmail.com>
2019-09-12 17:30:55 +0300

    #8 code is ok, but some issue with VK driver on thinkpad itself. postnote issue

--- platform_test/vk_vulkano_test0/src/main.rs
+++ platform_test/vk_vulkano_test0/src/main.rs
@@ -1,3 +1,18 @@
+use vulkano::instance::Instance;
+use vulkano::instance::InstanceExtensions;
+use vulkano::instance::PhysicalDevice;
+
 fn main() {
     println!("Hello, Vulkano!");
+
+    let instance = Instance::new(None, &InstanceExtensions::none(), None)
+        .expect("failed to create instance");
+
+    let physical = PhysicalDevice::enumerate(&instance).next()
+        .expect("no device available");
+
+    for family in physical.queue_families(){
+        println!("Found a queue famili with {:?} queue(s)", family.queues_count());
+    }
+
 }
