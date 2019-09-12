77f5e26c2b60ade6211c7f9ef1241679741b69dd
Andrey Yaromenok/PC <ayaromenok@gmail.com>
2019-09-12 10:35:07 +0300

    #10 working winit test. need to check it at other platforms

--- platform_test/winit_unix/src/main.rs
+++ platform_test/winit_unix/src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+extern crate winit;
+
+use std::collections::HashMap;
+
+
+fn main(){
+    let mut events_loop = winit::EventsLoop::new();
+    let mut windows = HashMap::new();
+
+    for _ in 0..3{
+        let window = winit::Window::new(&events_loop).unwrap();
+        windows.insert(window.id(), window);
+    }
+
+    println!("winit test - few empty windows");
+
+    events_loop.run_forever( |event| {
+        match event{
+            winit::Event::WindowEvent {
+                event: winit::WindowEvent::CloseRequested,
+                window_id
+            } => {
+                    println!("Window {:?} has recieved the signal to close", window_id);
+
+                    windows.remove(&window_id);
+                    if windows.is_empty(){
+                        println!("windows HashMap is empty, exiting...");
+                        return winit::ControlFlow::Break;
+                    }
+                }
+                _  => (),
+        }
+        winit::ControlFlow::Continue
+    })
+
 }
