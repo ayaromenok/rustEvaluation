40456aea65fbd0bc66050cec109b3ebe1be5e3ba
Andrey Yaromenok/PC <ayaromenok@gmail.com>
2019-09-23 23:00:56 +0300

    #8 vulkano tets can work on JetsonNano

--- platform_test/vk_vulkano_test0/src/main.rs
+++ platform_test/vk_vulkano_test0/src/main.rs
@@ -2,6 +2,10 @@ use vulkano::instance::Instance;
 use vulkano::instance::InstanceExtensions;
 use vulkano::instance::PhysicalDevice;
 
+use vulkano::device::Device;
+use vulkano::device::DeviceExtensions;
+use vulkano::device::Features;
+
 fn main() {
     println!("Hello, Vulkano!");
 
@@ -15,4 +19,14 @@ fn main() {
         println!("Found a queue famili with {:?} queue(s)", family.queues_count());
     }
 
+    let queue_family = physical.queue_families()
+        .find(|&q| q.supports_graphics())
+        .expect("could't find a graphical queue famili");
+
+    let (device, mut queues) = {
+        Device::new(physical, &Features::none(), &DeviceExtensions::none(),
+            [(queue_family, 0.5)].iter().cloned()).expect("failed to create device")
+    };
+
+    let queue = queues.next().unwrap();
 }
